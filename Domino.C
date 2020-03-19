efb7055f80aa2298faa8a138dd9cc8808de5198e
sergio-dp <sergio.depaulam@alumnos.upm.es>
2020-03-19 21:51:34 +0100

    Mejora de Domino.C
    He implementado una función que reparte las fichas entre los jugadores sin repetir ninguna

--- Domino.C
+++ Domino.C
@@ -2,10 +2,12 @@
 #include <time.h>
 #include <stdlib.h>
 
+void repartirfichas(int x[28][3], int f, int c); //esta función repartirá las fichas entre todos los jugadores
+
 int main()
 {
-	// creamos una matriz que contenga las 28 filas, siendo la primera columna el nÃºmero asociado a cada ficha
-	int fichas[28][3] = // Matriz 28 x 3
+	// creamos una matriz que contenga las 28 filas, siendo la primera columna el número asociado a cada ficha
+	int fichas[28][3] = // matriz 28 x 3
 	{
 		{0, 0, 0}, // fila 1
 		{1, 0, 1}, // fila 2
@@ -36,19 +38,56 @@ int main()
 		{26, 5, 6}, // fila 27
 		{27, 6, 6}  // fila 28
 	};
-	int a, i, j, jugadores;
-	printf("Selecciona el nÃºmero de jugadores: ");
-	scanf("%i", &jugadores);
+	repartirfichas(fichas, 28, 3);
+	return 0;
+}
+
+void repartirfichas(int x[28][3], int f, int c)
+{
+	int a, i, j, jugadores, igual;
+	int valores[28]; // vector donde voy a almacenar los valores de a
+	printf("Selecciona el número de jugadores: ");
+	scanf("%i", &jugadores); // introduzco el número de jugadores deseados
 	srand(time(NULL));
-	for(j=1; j<=jugadores; j++)
+	for(i=0; i<28; i++) //este bucle sirve para llenar el vector con valores aleatorios de a entre 0 y 27 sin repeticion
 	{
-		printf("Jugador %i: ", j);
-		for(i=1; i<=7;i++)
+		do
 		{
+			igual=0;
 			a=rand()%28;
-			printf("[%i|%i] ", fichas[a][1], fichas[a][2]);
+			for(j=0; j<i; j++)
+			{
+				if(a==valores[j])
+				igual=1;
+			}
+		}while(igual==1);
+		valores[i]=a;
+	}
+	for(j=1; j<=jugadores; j++) // repartirá fichas a tantos jugadores como le haya indicado
+	{
+		printf("Jugador %i: ", j); //fichas correspondientes al jugador j (1, 2, 3 o 4)
+		for(i=1; i<=7;i++)  //repito el bucle 7 veces pues ak comenzar la partida cada jugador tiene 7 fichas
+		{
+			switch(j)
+			{
+				case 1: //reparto al jugador 1
+					a=valores[i-1];
+					printf("[%i|%i] ", x[a][1], x[a][2]);
+					break;
+				case 2: //reparto al jugador 2
+					a=valores[i+6];
+					printf("[%i|%i] ", x[a][1], x[a][2]);
+					break;
+				case 3: //reparto al jugador 3
+					a=valores[i+13];
+					printf("[%i|%i] ", x[a][1], x[a][2]);
+					break;
+				case 4: //reparto al jugador 4
+					a=valores[i+20];
+					printf("[%i|%i] ", x[a][1], x[a][2]);
+					break;
+			}
 		}
 		printf("\n");
 	}
-	return 0;
 }
