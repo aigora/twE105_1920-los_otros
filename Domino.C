e584066f2b9b3446571908931d41dddbd7538f20
sergio-dp <sergio.depaulam@alumnos.upm.es>
2020-03-20 18:55:07 +0100

    Mejora Dominó.C
    He introducido una función que indica quien es el que tiene el primer turno (el que tiene el 6 doble).

--- Domino.C
+++ Domino.C
@@ -2,12 +2,12 @@
 #include <time.h>
 #include <stdlib.h>
 
-void repartirfichas(int x[28][3], int f, int c); //esta función repartirá las fichas entre todos los jugadores
+int primerturno(int x[], int y[], int z[], int v[], int n);
 
 int main()
 {
-	// creamos una matriz que contenga las 28 filas, siendo la primera columna el número asociado a cada ficha
-	int fichas[28][3] = // matriz 28 x 3
+	int fichas[28][3] = // matriz 28 x 3, siendo la primera columna el número asociado a cada ficha
+	                    // la segunda y tercera columna forman todas las combinaciones posibles de fichas
 	{
 		{0, 0, 0}, // fila 1
 		{1, 0, 1}, // fila 2
@@ -38,56 +38,100 @@ int main()
 		{26, 5, 6}, // fila 27
 		{27, 6, 6}  // fila 28
 	};
-	repartirfichas(fichas, 28, 3);
-	return 0;
-}
-
-void repartirfichas(int x[28][3], int f, int c)
-{
-	int a, i, j, jugadores, igual;
-	int valores[28]; // vector donde voy a almacenar los valores de a
+	int a; // esta variable va a servir para el reparto inicial de fichas
+	int igual; // esta variable va a servir para el reparto inicial de fichas
+	int i, j; // estas dos variables las vamos a utilizar para los bucles
+	int jugadores; // esta variable va a servir para indicar el número de jugadores, hasta un máximo de 4
+	int valores[28]; // vector donde voy a almacenar todos los valores de a
+	int fichasjugador1[7]; // vector donde voy a almacenar las fichas del jugador 1
+	int fichasjugador2[7]; // vector donde voy a almacenar las fichas del jugador 2
+	int fichasjugador3[7]; // vector donde voy a almacenar las fichas del jugador 3
+	int fichasjugador4[7]; // vector donde voy a almacenar las fichas del jugador 4
+	int turno1;
 	printf("Selecciona el número de jugadores: ");
 	scanf("%i", &jugadores); // introduzco el número de jugadores deseados
 	srand(time(NULL));
-	for(i=0; i<28; i++) //este bucle sirve para llenar el vector con valores aleatorios de a entre 0 y 27 sin repeticion
+
+	//este bucle sirve para llenar el vector con valores aleatorios de a entre 0 y 27 sin repeticion
+	for(i=0; i<28; i++)
 	{
-		do
+		do // uso un do-while porque necesito que, al menos, se genere 1 número aleatorio
 		{
 			igual=0;
 			a=rand()%28;
 			for(j=0; j<i; j++)
 			{
-				if(a==valores[j])
+				if(a==valores[j]) // si algún valor de a se repite, el bucle do-while se volverá a ejecutar
 				igual=1;
 			}
 		}while(igual==1);
 		valores[i]=a;
 	}
-	for(j=1; j<=jugadores; j++) // repartirá fichas a tantos jugadores como le haya indicado
+	
+	//este bucle sirve para repatir las fichas entre el número indicado de jugadores
+	for(j=1; j<=jugadores; j++)
 	{
 		printf("Jugador %i: ", j); //fichas correspondientes al jugador j (1, 2, 3 o 4)
-		for(i=1; i<=7;i++)  //repito el bucle 7 veces pues ak comenzar la partida cada jugador tiene 7 fichas
+		for(i=1; i<=7;i++)  //repito el bucle 7 veces pues al comenzar la partida cada jugador tiene 7 fichas
 		{
 			switch(j)
 			{
 				case 1: //reparto al jugador 1
 					a=valores[i-1];
-					printf("[%i|%i] ", x[a][1], x[a][2]);
+					fichasjugador1[i-1]=a; //asocio a cada posición del vector una ficha
+					printf("[%i|%i] ", fichas[a][1], fichas[a][2]);
 					break;
 				case 2: //reparto al jugador 2
 					a=valores[i+6];
-					printf("[%i|%i] ", x[a][1], x[a][2]);
+					fichasjugador2[i-1]=a;
+					printf("[%i|%i] ", fichas[a][1], fichas[a][2]);
 					break;
 				case 3: //reparto al jugador 3
 					a=valores[i+13];
-					printf("[%i|%i] ", x[a][1], x[a][2]);
+					fichasjugador3[i-1]=a;
+					printf("[%i|%i] ", fichas[a][1], fichas[a][2]);
 					break;
 				case 4: //reparto al jugador 4
 					a=valores[i+20];
-					printf("[%i|%i] ", x[a][1], x[a][2]);
+					fichasjugador4[i-1]=a;
+					printf("[%i|%i] ", fichas[a][1], fichas[a][2]);
 					break;
 			}
 		}
 		printf("\n");
 	}
+	turno1=primerturno(fichasjugador1, fichasjugador2, fichasjugador3, fichasjugador4, jugadores);
+	printf("Es el turno del jugador %i", turno1);
+	return 0;
+}
+
+int primerturno(int x[], int y[], int z[], int v[], int n)
+{
+	int i, j, turno1;
+	for(j=1; j<=n; j++)
+	{
+		for(i=0; i<7; i++)
+		{
+			switch(j)
+			{
+				case 1:
+					if(x[i]==27) //compruebo que jugador tiene el 6 doble
+					turno1=1;
+					break;
+				case 2:
+					if(y[i]==27)
+					turno1=2;
+					break;
+				case 3:
+					if(z[i]==27)
+					turno1=3;
+					break;
+				case 4:
+					if(v[i]==27)
+					turno1=4;
+					break;
+			}
+		}
+	}
+	return turno1;
 }
