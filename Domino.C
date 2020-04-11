383829720e507d538cfe177873e3030e7d8e973d
sergio-dp <sergio.depaulam@alumnos.upm.es>
2020-04-11 13:04:44 +0200

    Update Domino.C

--- Domino.C
+++ Domino.C
@@ -8,22 +8,21 @@ typedef struct
 	int numero2;
 } ficha;
 
-int fichaalta(int fichasjugador1[], int fichasjugador2[], int fichasjugador3[], int fichasjugador4[], int jugadores);
-int primerturno(int fichasjugador1[], int fichasjugador2[], int fichasjugador3[], int fichasjugador4[], int jugadores, int fichagrande);
-int siguienteturno(int jugadores, int turno);
-int comprueba(int fichasjugador1[], int fichasjugador2[], int fichasjugador3[], int fichasjugador4[], int final, int turno);
-int modofacil(ficha fichas[], int jugador[], int tablero[], int contadortablero, int repeticion);
-int jugadorlocal(ficha fichas[], int fichasjugador1[], int tablero[], int contadortablero, int contadorpozo, int repeticion);
-int analizaderecha(ficha fichas[], int fichasjugador1[], int tablero[], int contadortablero, int posibilidad, int i);
-int analizaizquierda(ficha fichas[], int fichasjugador1[], int tablero[], int posibilidad, int i);
-
-void imprimirnormal(ficha fichas[], int vectorfichas[], int posicion); // imprime una ficha tal y como está en la matriz
-void imprimirreves(ficha fichas[], int vectorfichas[], int posicion); // imprime una ficha dada la vuelta, por ejemplo, la ficha [1|2] la imprime como [2|1]
-void juego(ficha fichas[], int fichasjugador1[], int fichasjugador2[], int fichasjugador3[], int fichasjugador4[], int tablero[], int pozo[], int jugadores, int turno, int final);
-void modomultijugador(int jugadores, int dificultad); // esta función va a contener todo el sistema de juego
-void robarficha(int jugador[], int pozo[], int contadorpozo);
-void colocarderecha(ficha fichas[], int fichasjugador1[], int tablero[], int contadortablero, int eleccionficha);
-void colocarizquierda(ficha fichas[], int fichasjugador1[], int tablero[], int contadortablero, int eleccionficha);
+void modomultijugador(int jugadores, int dificultad); // esta funcion va a contener todo el sistema de juego
+	int fichaalta(int fichasjugador1[], int fichasjugador2[], int fichasjugador3[], int fichasjugador4[], int jugadores);
+	int primerturno(int fichasjugador1[], int fichasjugador2[], int fichasjugador3[], int fichasjugador4[], int jugadores, int fichagrande);
+	void juego(ficha fichas[], int fichasjugador1[], int fichasjugador2[], int fichasjugador3[], int fichasjugador4[], int tablero[], int pozo[], int jugadores, int turno, int final);
+		void imprimirnormal(ficha fichas[], int vectorfichas[], int posicion); 
+		void imprimirreves(ficha fichas[], int vectorfichas[], int posicion);
+	int siguienteturno(int jugadores, int turno);
+	int jugadorlocal(ficha fichas[], int fichasjugador1[], int tablero[], int contadortablero, int contadorpozo, int repeticion);
+		int analizaderecha(ficha fichas[], int fichasjugador1[], int tablero[], int contadortablero, int posibilidad, int i);
+		int analizaizquierda(ficha fichas[], int fichasjugador1[], int tablero[], int posibilidad, int i);
+		void colocarderecha(ficha fichas[], int fichasjugador1[], int tablero[], int contadortablero, int eleccionficha);
+		void colocarizquierda(ficha fichas[], int fichasjugador1[], int tablero[], int contadortablero, int eleccionficha);
+	void robarficha(int jugador[], int pozo[], int contadorpozo);
+	int modofacil(ficha fichas[], int jugador[], int tablero[], int contadortablero, int repeticion);
+	int comprueba(int fichasjugador1[], int fichasjugador2[], int fichasjugador3[], int fichasjugador4[], int final, int turno);
 
 int main()
 {
